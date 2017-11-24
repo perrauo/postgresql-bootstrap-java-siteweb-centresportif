14812e0c8a2dcde949014f902863dc7a46e7ebd8
oliperraul <olivier.perrault@usherbrooke.ca>
2017-11-24 14:25:55 -0500

    working on view that brings the closest station to a catastrophe

--- SQL/TS_cre.sql
+++ SQL/TS_cre.sql
@@ -29,27 +29,27 @@ Email : Samuel.Z.Lapointe@usherbrooke.ca
 BEGIN CREATE DOMAINS
 */----
 
-CREATE DOMAIN Geo_Id
+CREATE DOMAIN d_Geo_Id
   CHAR(8)
   CHECK (VALUE SIMILAR TO '[A-Z]{2}[0-9]{6}'); --(?:AB|BC|...) other method to code maybe-- 2 lettre (liste des province) suivie de 6 chiffre
 
-CREATE DOMAIN Prov
+CREATE DOMAIN d_Prov
   VARCHAR(30)
   CHECK (VALUE IN ('AB','BC','MB','NB','NL','NT','NS','NU','ON','PE','QC','SK','YT'));--LISTE OFFICIELLE
 
-CREATE DOMAIN Lat_range
+CREATE DOMAIN d_Lat_range
   CHAR(3)
   CHECK ((VALUE SIMILAR TO '[0-9]{2}N') AND (SUBSTR(VALUE,1,2)::DECIMAL(7,4) BETWEEN 42 AND 83));--CAST AS????--as per stat can, range from 42N to 83N
 
-CREATE DOMAIN Lon_range
+CREATE DOMAIN d_Lon_range
   CHAR(3)
   CHECK ((VALUE SIMILAR TO '[0-9]{2}W') AND (SUBSTR(VALUE,1,2)::DECIMAL(7,4) BETWEEN 53 AND 141));--CAST AS????-- same but from 53W to 141W
 
-CREATE DOMAIN Cata_Name
+CREATE DOMAIN d_Cata_Name
   VARCHAR(20)
   CHECK (VALUE IN ('Avalanche','Cold event','Drought','Flood','Geomagnetic storm','Heat event','Hurricane','Storm surge','Storm surge','Thunderstorms','Tornado','Wildfire','Winter storm'));
 
-CREATE DOMAIN Date_eco
+CREATE DOMAIN d_Date_eco
   DATE
   CHECK (VALUE >= '1900-01-01');
 
@@ -57,23 +57,23 @@ CREATE DOMAIN Temp --en degrees celcius
   NUMERIC(4,1)
   CHECK (VALUE BETWEEN -100 AND 60); --a l'interieur des records terrestres
 
-CREATE DOMAIN Wind_v --en km/h
+CREATE DOMAIN d_Wind_v --en km/h
   SMALLINT
   CHECK (VALUE BETWEEN 0 AND 450); --la plus forte rafale jamais repertoriée
 
-CREATE DOMAIN Cardinal
+CREATE DOMAIN d_Cardinal
   CHAR(2)
   CHECK (VALUE IN ('N','NNE','NE','ENE','E','ESE','SE','SSE','S','SSW','SW','WSW','W','WNW','NW','NNW')); --PER CONVENTION
 
-CREATE DOMAIN Rain --in mm
+CREATE DOMAIN d_Rain --in mm
   NUMERIC(5,1)
   CHECK (VALUE BETWEEN 0 AND 2000); --a l'interieur des records enregistrees dans le monde avec marge d'environ 10%
 
-CREATE DOMAIN Snow -- in cm
+CREATE DOMAIN d_Snow -- in cm
   NUMERIC(4,1)
   CHECK (VALUE BETWEEN 0 AND 200); --a l'interieur des records enregistrees dans le monde (78.5 pouce * 2.54)
 
-CREATE DOMAIN Cata_Id
+CREATE DOMAIN d_Cata_Id
   INT
   CHECK (VALUE BETWEEN 0 AND 999999);--1 MILLION POSSIBLE DISASTER IN BD LIFETIME
 
@@ -81,19 +81,19 @@ CREATE DOMAIN Cost
   INT
   CHECK (VALUE BETWEEN 0 AND 100000000000);--100 MILLIARDS CAN DOLLAR IS ENOUGH
 
-CREATE DOMAIN People
+CREATE DOMAIN d_People
   INT
   CHECK (VALUE BETWEEN 0 AND 50000000); --30% MORE THAN ACTUAL POPULATION
 
-CREATE DOMAIN Richter
+CREATE DOMAIN d_Richter
   NUMERIC(2,1)
   CHECK (VALUE BETWEEN 1.0 AND 9.9);--Richter scale
 
-CREATE DOMAIN Year_rate
+CREATE DOMAIN d_Year_rate
   SMALLINT
   CHECK (VALUE BETWEEN 1914 AND extract(YEAR FROM now()));--DATA not available prior to 1914
 
-CREATE DOMAIN Inflation_rate
+CREATE DOMAIN d_Inflation_rate
   NUMERIC(4,4)
   CHECK (VALUE BETWEEN 0.7 AND 1.3);--IN RANGE OF HISTORICAL LOWEST (-17.8%) AND HIGHEST 21.6%) http://www.inflation.eu
 
@@ -111,32 +111,32 @@ BEGIN CREATE TABLES
 */----
 
 CREATE TABLE LOCATION (
-  location_id Geo_Id NOT NULL,
+  location_id d_geo_id NOT NULL,
   location_name VARCHAR(80) NOT NULL,--no longer name in CANADA
-  province Prov NOT NULL,
-  latitude Lat_range NOT NULL,
-  longitude Lon_range NOT NULL,
+  province d_prov NOT NULL,
+  latitude d_lat_range NOT NULL,
+  longitude d_lon_range NOT NULL,
   CONSTRAINT LOCATION_cc0 PRIMARY KEY (location_id)
 );
 
 CREATE TABLE DISASTER_TYPE (
-  type_name Cata_Name NOT NULL,
+  type_name d_cata_name NOT NULL,
   information VARCHAR(80) NOT NULL,
   CONSTRAINT DISASTER_TYPE_cc0 PRIMARY KEY (type_name)
 );
 
 CREATE TABLE WEATHER_STATION (
-  station_id Geo_id NOT NULL,
+  station_id d_geo_id NOT NULL,
   station_name VARCHAR(80) NOT NULL,
-  province Prov NOT NULL,
-  latitude Lat_range NOT NULL,
-  longitude Lon_range NOT NULL,
+  province d_prov NOT NULL,
+  latitude d_lat_range NOT NULL,
+  longitude d_lon_range NOT NULL,
   PRIMARY KEY (station_id)
 );
 
 CREATE TABLE TEMPERATURE (
-  _date Date_eco NOT NULL,
-  station_id Geo_Id NOT NULL,
+  _date d_date_eco NOT NULL,
+  station_id d_geo_Id NOT NULL,
   temp_min Temp NOT NULL,
   temp_max Temp NOT NULL,
   CONSTRAINT TEMPERATURE_cc0 PRIMARY KEY (_date, station_id),
@@ -144,49 +144,49 @@ CREATE TABLE TEMPERATURE (
 );
 
 CREATE TABLE WIND (
-  _date Date_eco NOT NULL,
-  station_id Geo_id NOT NULL,
-  gust_max Wind_v NOT NULL,
-  gust_direction Cardinal NOT NULL,
+  _date d_date_eco NOT NULL,
+  station_id d_geo_id NOT NULL,
+  gust_max d_wind_v NOT NULL,
+  gust_direction d_cardinal NOT NULL,
   CONSTRAINT WIND_cc0 PRIMARY KEY (_date, station_id)
 );
 
 CREATE TABLE PRECIPITATION (
-  _date Date_eco NOT NULL,
-  station_id Geo_Id NOT NULL,
-  prec_rain Rain NOT NULL,
-  prec_snow Snow NOT NULL,
-  prec_total Rain NOT NULL,
+  _date d_date_eco NOT NULL,
+  station_id d_geo_id NOT NULL,
+  prec_rain d_rain NOT NULL,
+  prec_snow d_snow NOT NULL,
+  prec_total d_rain NOT NULL,
   CONSTRAINT PRECIPITATION_cc0 PRIMARY KEY (_date, station_id),
   CONSTRAINT PRECIPITATION_ce0 CHECK(prec_rain+(prec_snow*10)=prec_total)
 );
 
 CREATE TABLE DISASTER (
-  disaster_id Cata_Id NOT NULL,
-  date_debut Date_eco NOT NULL,
-  type_name Cata_Name NOT NULL,
-  estimated_cost Cost NOT NULL,
-  nb_casualties People NOT NULL,
-  nb_wounded People NOT NULL,
-  nb_deaths People NOT NULL,
-  nb_evacuees People NOT NULL,
-  magnitude Richter NOT NULL,
+  disaster_id d_cata_Id NOT NULL,
+  date_debut d_date_eco NOT NULL,
+  type_name d_cata_name NOT NULL,
+  estimated_cost cost NOT NULL,
+  nb_casualties d_people NOT NULL,
+  nb_wounded d_people NOT NULL,
+  nb_deaths d_people NOT NULL,
+  nb_evacuees d_people NOT NULL,
+  magnitude d_Richter NOT NULL,
   CONSTRAINT DISASTER_CC0 PRIMARY KEY (disaster_id),
   CONSTRAINT TEMPERATURE_fk0 FOREIGN KEY (type_name) REFERENCES DISASTER_TYPE (type_name)
 );
 
 CREATE TABLE DISASTER_LOCATION (
-  disaster_id Cata_Id NOT NULL,
-  _date Date_eco NOT NULL,
-  location_id Geo_Id NOT NULL,
+  disaster_id d_Cata_Id NOT NULL,
+  _date d_Date_eco NOT NULL,
+  location_id d_Geo_Id NOT NULL,
   CONSTRAINT DISASTER_LOCATION_CC0 PRIMARY KEY (disaster_id, _date, location_id),
   CONSTRAINT DISASTER_LOCATION_fk0 FOREIGN KEY (disaster_id) REFERENCES DISASTER (disaster_id)
 );
 
 
 CREATE TABLE INFLATION_RATE (
-  _year Year_rate NOT NULL,
-  inflation Inflation_rate NOT NULL,
+  _year d_Year_rate NOT NULL,
+  inflation d_inflation_rate NOT NULL,
   CONSTRAINT INFLATION_RATE_CC0 PRIMARY KEY (_year)
 );
 
@@ -201,70 +201,28 @@ END CREATE TABLES
 /*----
 -----------------
 -----------------
-BEGIN CREATE VIEWS
-*/----
-
---devrait etre une view ou une requete avec calcul de distance par rapport a la station meteo la plus proche
-CREATE OR REPLACE VIEW DISASTER_METEO AS
-  SELECT 
-  disaster_id,
-  type_name,
-  _date,
-  location_id,
-  temp_min,
-  temp_max,
-  gust_max,
-  gust_direction,
-  prec_rain,
-  prec_snow,
-  prec_total,
-  FROM DISASTER JOIN WEATHER_CONDITION
-    JOIN WIND USING (_date)
-    JOIN PRECIPITATION USING (_date)
-;;
-
-
-
---était clairement une vue comme dans le tp sur carnet meteo
-CREATE OR REPLACE VIEW WEATHER_CONDITION AS
-  SELECT _date,
-    station_id,
-    temp_min,
-    temp_max,
-    gust_max,
-    gust_direction,
-    prec_rain,
-    prec_snow,
-    prec_total
-  FROM TEMPERATURE
-    JOIN WIND USING (_date)
-    JOIN PRECIPITATION USING (_date)
-;
-
-/*----
-END CREATE VIEWS
------------------
------------------
-*/----
-
-
-
-/*----
------------------
------------------
 BEGIN CREATE FUNCTIONS
 */----
 
-CREATE OR REPLACE FUNCTION is_ensol(e varchar) returns boolean as $$
-begin
-  perform e::INT;
-  IF e IS NOT NULL AND e::INT BETWEEN 0 AND 1440 THEN
-     RETURN true;
-  END IF;
-  RETURN false;
-exception when others then
-  RETURN false;
-end;
+CREATE OR REPLACE FUNCTION get_dist(x1 DECIMAL(7,4), y1 DECIMAL(7,4), x2 DECIMAL(7,4), y2 DECIMAL(7,4)) RETURNS DECIMAL(7,4) AS
+$$
+BEGIN
+  RETURN sqrt(power(x2-x1, 2)+power(y2-y1, 2));
+END;
+$$ language plpgsql;
+
+CREATE OR REPLACE FUNCTION lanum(lat_ran d_Lat_range) RETURNS DECIMAL(7,4) AS
+$$
+BEGIN
+  RETURN (SUBSTR(VALUE,1,2)::DECIMAL(7,4));
+END;
+$$ language plpgsql;
+
+CREATE OR REPLACE FUNCTION lonnum(lat_ran d_lon_range) RETURNS DECIMAL(7,4) AS
+$$
+BEGIN
+  RETURN (SUBSTR(VALUE,1,2)::DECIMAL(7,4));
+END;
 $$ language plpgsql;
 
 /*----
@@ -331,13 +289,12 @@ END CREATE PROCEDURES
 BEGIN CREATE TRIGGERS
 */----
 
-
-CREATE TRIGGER correct_id
+/*CREATE TRIGGER correct_id
 BEFORE
 INSERT
 ON
 FOR EACH ROW
-EXECUTE PROCEDURE valid_data();
+EXECUTE PROCEDURE valid_data();*/
 
 
 /*----
@@ -345,3 +302,65 @@ END CREATE TRIGGERS
 -----------------
 -----------------
 */----
+
+
+/*----
+-----------------
+-----------------
+BEGIN CREATE VIEWS
+*/----
+
+--devrait etre une view ou une requete avec calcul de distance par rapport a la station meteo la plus proche
+CREATE OR REPLACE VIEW DISASTER_METEO AS
+  WITH
+  disaster_closest_stations AS -- Plants ayant plus de trois observations
+  (
+    SELECT * FROM
+    (DISASTER_LOCATION JOIN LOCATION USING (location_id)) AS A
+    CROSS JOIN WEATHER_STATION AS B
+    WHERE NOT EXISTS(  --WHERE DOES NOT EXIST A METEO STATION THAT IS CLOSER
+        SELECT longitude, latitude
+        FROM WEATHER_STATION AS other
+        WHERE
+            get_dist(lanum(A.latitude), lonnum(A.longitude),
+                 lanum(other.latitude), lonnum(other.longitude))
+        <
+            get_dist(lanum(A.latitude), lonnum(A.longitude),
+                 lanum(B.latitude), lonnum(B.longitude))
+  )
+  SELECT
+  disaster_id,
+  disaster_closest_stations._date,
+  location_id,
+  temp_min,
+  temp_max,
+  gust_max,
+  gust_direction,
+  prec_rain,
+  prec_snow,
+  prec_total
+
+
+
+--était clairement une vue comme dans le tp sur carnet meteo
+CREATE OR REPLACE VIEW WEATHER_CONDITION AS
+  SELECT _date,
+    station_id,
+    temp_min,
+    temp_max,
+    gust_max,
+    gust_direction,
+    prec_rain,
+    prec_snow,
+    prec_total
+  FROM TEMPERATURE
+    JOIN WIND USING (_date)
+    JOIN PRECIPITATION USING (_date)
+;
+
+/*----
+END CREATE VIEWS
+-----------------
+-----------------
+*/----
+
